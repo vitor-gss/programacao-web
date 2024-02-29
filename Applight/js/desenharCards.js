29342ee0553b64a75cd07b5147c4172e6cbcff48
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2024-02-29 19:12:17 -0300

    a

--- Applight/js/desenharCards.js
+++ Applight/js/desenharCards.js
@@ -5,16 +5,16 @@ function desenharCards(idContainer, arrayCards) {
     containerCards.innerHTML = ``
     for (const vagaDeEmprego of arrayCards) {
         containerCards.innerHTML += `
-        <div class="card g-2 col-12 col-md-5" onclick="chamarDetalhes(${vagaDeEmprego.id})" style="width: 18rem;">
+        <div class="card g-1 col-12 col-md-3" onclick="chamarDetalhes(${vagaDeEmprego.id})" >
             <div class="row g-0">
                 <div class="col-4 col-md-12 align-items-center justify-content-center d-flex">
-                    <img src="${vagaDeEmprego.img}" class="img-fluid rounded ajuste-img ms-2 border card-md-img-top" alt="">
+                    <img src="${vagaDeEmprego.img}" class="img-fluid rounded ajuste-img ms-2 m-md-0 border card-md-img-top" alt="">
                 </div>
                 <div class="col-8">
                     <div class="card-body">
                         <h5 class="card-title">${vagaDeEmprego.nome}</h5>
                         <p class="card-text">${vagaDeEmprego.empresa}</p>
-                        <p class="card-text mt-5"><small class="text-body-secondary">${(vagaDeEmprego.salario).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</small></p>
+                        <p class="card-text"><small class="text-body-secondary">${(vagaDeEmprego.salario).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</small></p>
                     </div>
                 </div>
             </div>
