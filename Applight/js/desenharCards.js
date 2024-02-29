05ebd5c4f00d1405fe0f8dd572703e43e1abb34c
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2024-02-29 16:22:45 -0300

    Sim

--- Applight/js/desenharCards.js
+++ Applight/js/desenharCards.js
@@ -5,10 +5,10 @@ function desenharCards(idContainer, arrayCards) {
     containerCards.innerHTML = ``
     for (const vagaDeEmprego of arrayCards) {
         containerCards.innerHTML += `
-        <div class="card mb-3 col-12 col-md-5" onclick="chamarDetalhes(${vagaDeEmprego.id})">
+        <div class="card g-2 col-12 col-md-5" onclick="chamarDetalhes(${vagaDeEmprego.id})" style="width: 18rem;">
             <div class="row g-0">
-                <div class="col-4">
-                    <img src="${vagaDeEmprego.img}" class="img-fluid rounded-start ajuste-img" alt="">
+                <div class="col-4 col-md-12 align-items-center justify-content-center d-flex">
+                    <img src="${vagaDeEmprego.img}" class="img-fluid rounded ajuste-img ms-2 border card-md-img-top" alt="">
                 </div>
                 <div class="col-8">
                     <div class="card-body">
---
+++ Applight/js/header-user.js
@@ -0,0 +1,14 @@
+const header = document.querySelector('#header')
+
+header.innerHTML += `	<section class="banner mb-5">
+<nav class="navbar navbar-expand-lg">
+</nav>
+<div class="d-flex container justify-content-between">
+    <a class="navbar-brand navbar-logo logo" href="./index.html"> <img src="vixz.png" alt="logo" class="logo-1"> </a>
+    <a class="navbar-brand navbar-logo d-flex flex-column user align-items-center perfil" href="./perfil.html">
+        <i class="bi bi-person-circle" style="font-size: 50px; color: white;"></i>
+        <p style="color: white; font-weight: bold;">Perfil</p>
+    </a>
+</div>
+</section>`
+
