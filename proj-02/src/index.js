a765cfc5618f7b3fd27540035ad4bfa025f513a7
Vitor Gabriel da Silva Santos <126255280+vitor-gss@users.noreply.github.com>
2024-04-17 09:23:38 -0300

    extended true

--- proj-02/src/index.js
+++ proj-02/src/index.js
@@ -3,8 +3,8 @@ const app = express()
 
 const port = 3000
 
-// app.use(express.json())
-app.listen(express.urlencoded())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res)=> {
 	res.send(`Hello Vitor!`)
@@ -19,4 +19,3 @@ app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
 })
 
-// Projeto do vitu Vytuua
