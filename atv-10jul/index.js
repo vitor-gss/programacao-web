3b193ba390d3d9a22c34f83b9ae77e11e0bccb86
Vitor Gabriel da Silva Santos <126255280+vitor-gss@users.noreply.github.com>
2024-07-10 10:20:51 -0300

    Arquivo

--- atv-10jul/index.js
+++ atv-10jul/index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// GET
 app.get('/', (req, res) => {
-  res.send('Vitor World!')
-})
+  res.send('Rota GET: Olá, Mundo!');
+});
 
+app.get('/teste', (req, res) => {
+  res.send('Rota de teste: Oi');
+});
+
+// POST
+app.post('/', (req, res) => {
+  const mensagem = req.body.mensagem;
+  res.send(`Rota POST: Mensagem: "${mensagem}"`);
+});
+
+// Inicia o servidor
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Aplicação rodando em http://localhost:${port}`);
+});
