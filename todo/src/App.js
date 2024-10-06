da8485fc4da2d8ccc956cb4880f5e5ef9423e3c7
Vitor Gabriel da Silva Santos <126255280+vitor-gss@users.noreply.github.com>
2024-10-06 20:41:13 -0300

    a

---
+++ todo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <Todo> Todo </Todo>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/services/firebase.config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6SCrqn8dKUa7ZVxDei5hDapxQ4s39t2I",
+  authDomain: "web-s-4c22d.firebaseapp.com",
+  projectId: "web-s-4c22d",
+  storageBucket: "web-s-4c22d.appspot.com",
+  messagingSenderId: "353483137827",
+  appId: "1:353483137827:web:0fe9843c43ab1259086296",
+  measurementId: "G-SQPQ1JNBRP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
